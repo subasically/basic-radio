1b17dec2137e293480208fdbb7fbb84d186c649c
Alen Subasic <alen.subasic@dhigroupinc.com>
2024-10-06 15:13:09 -0500

    fixing urls

--- app.vue
+++ app.vue
@@ -17,20 +17,20 @@ const items = [{
     <UTabs :items="items" class="w-full">
       <template #narodna="{ item }">
         <UCard>
-          <RadioPlayer stationUrl="https://basic-radio.subasically.me/listen/narodna_muzika/narodna"
+          <RadioPlayer stationUrl="https://basic-radio.subasically.me/listen/narodna_muzika/radio"
             stationName="narodna" />
         </UCard>
       </template>
 
       <template #pop="{ item }">
         <UCard>
-          <RadioPlayer stationUrl="https://basic-radio.subasically.me/listen/pop_muzika/pop" stationName="pop" />
+          <RadioPlayer stationUrl="https://basic-radio.subasically.me/listen/pop_muzika/radio" stationName="pop" />
         </UCard>
       </template>
 
       <template #trending="{ item }">
         <UCard>
-          <RadioPlayer stationUrl="https://basic-radio.subasically.me/listen/trending_muzika/trending"
+          <RadioPlayer stationUrl="https://basic-radio.subasically.me/listen/trending_muzika/radio"
             stationName="trending" />
         </UCard>
       </template>
